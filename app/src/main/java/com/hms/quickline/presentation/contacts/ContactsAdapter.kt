8383109d767493f84f4d6ca4ecf973cbb3e5f662
d00625778 <damla.yagmur1@huawei.com>
2022-05-23 10:16:49 +0300

    Com-25 contacts' photos updated

--- app/src/main/java/com/hms/quickline/presentation/contacts/ContactsAdapter.kt
+++ app/src/main/java/com/hms/quickline/presentation/contacts/ContactsAdapter.kt
@@ -1,5 +1,6 @@
 package com.hms.quickline.presentation.contacts
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -26,13 +27,14 @@ class ContactsAdapter(list: ArrayList<Users>, listener: ICallDialogAdapter) :
 
     inner class ViewHolder(binding: CardCallDialogBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
         fun bind(item: Users) {
             with(binding) {
 
                 Glide.with(this.root.context)
-                    .load("https://media-exp1.licdn.com/dms/image/D4D03AQEweV5ra2apTw/profile-displayphoto-shrink_800_800/0/1630667862366?e=1658361600&v=beta&t=qlNpziZO8fxddUwj5eiVQYygZJA0tNHNdFZTkBbdg-A")
+                    .load(item.photo)
                     .circleCrop()
-                    .into(imgProfilPhoto)
+                    .into(imgProfilePhoto)
 
                 tvName.text = item.name
                 if (item.isAvailable) {
