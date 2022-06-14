0a8cd5bfbea6b35538d2d80bfda64441114d92a6
s84251783 <sinan.yilmaz5@huawei.com>
2022-06-14 10:30:04 +0300

    Video Quality fixed, CloudDB zone close when app onDestroy

--- app/src/main/java/com/hms/quickline/core/util/Constants.kt
+++ app/src/main/java/com/hms/quickline/core/util/Constants.kt
@@ -4,9 +4,9 @@ object Constants {
 
     const val CloudDbZoneName = "COMDBZone"
 
-    const val VIDEO_WIDTH = 280
-    const val VIDEO_HEIGHT = 200
-    const val VIDEO_FPS = 60
+    const val VIDEO_WIDTH = 1280
+    const val VIDEO_HEIGHT = 720
+    const val VIDEO_FPS = 30
 
     const val LOCAL_TRACK_ID = "local_track"
     const val LOCAL_STREAM_ID = "stream_track"
