933d7f4c945ae41cb68925919c859f6e5453da76
b84251775 <123ewqaz!>
2022-07-27 10:22:29 +0300

    COM-49: When called pair decline the call,caller leave the call screen

--- app/src/main/java/com/hms/quickline/service/ActionReceiver.kt
+++ app/src/main/java/com/hms/quickline/service/ActionReceiver.kt
@@ -10,12 +10,15 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.hms.quickline.core.util.Constants
 import com.hms.quickline.core.util.Constants.ANSWER
 import com.hms.quickline.core.util.Constants.DECLINE
 import com.hms.quickline.core.util.Constants.MEETING_ID
 import com.hms.quickline.core.util.Constants.IS_JOIN
 import com.hms.quickline.core.util.Constants.UID
+import com.hms.quickline.data.model.CallsSdp
 import com.hms.quickline.data.model.Users
+import com.hms.quickline.data.webrtc.WebRtcClient
 import com.hms.quickline.ui.call.VideoCallActivity
 import com.hms.quickline.domain.repository.CloudDbWrapper
 import com.huawei.agconnect.cloud.database.CloudDBZone
@@ -47,6 +50,17 @@ class ActionReceiver : BroadcastReceiver() {
             DECLINE -> {
                 notificationUtils.getManager().cancel(150)
 
+                val callsSdp = CallsSdp()
+                callsSdp.meetingID = uid
+                callsSdp.callType = Constants.TYPE.END.name
+                val upsertTask = cloudDBZone?.executeUpsert(callsSdp)
+
+                upsertTask?.addOnSuccessListener { cloudDBZoneResult ->
+                    Log.i(TAG, "Calls Sdp Upsert success: $cloudDBZoneResult")
+                }?.addOnFailureListener {
+                    Log.e(TAG, "Calls Sdp Upsert failed: ${it.message}")
+                }
+
                 uid?.let { id ->
                     CloudDbWrapper.getUserById(id,object: CloudDbWrapper.ICloudDbWrapper{
                         override fun onUserObtained(users: Users) {
@@ -56,7 +70,7 @@ class ActionReceiver : BroadcastReceiver() {
                             upsertTask?.addOnSuccessListener { cloudDBZoneResult ->
                                 Log.i(TAG, "Calls UserCalling Upsert success: $cloudDBZoneResult")
                             }?.addOnFailureListener { exp->
-                                Log.i(TAG, "Calls UserCalling Upsert failed: ${exp.message}")
+                                Log.e(TAG, "Calls UserCalling Upsert failed: ${exp.message}")
                             }
                         }
                     })
