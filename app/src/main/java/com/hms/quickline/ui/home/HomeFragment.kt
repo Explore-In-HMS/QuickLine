382cdaf3d93f3e05b7d84de280188d28e890ab88
d00625778 <damla.yagmur1@huawei.com>
2022-07-28 19:12:32 +0300

    Com-51 Update Profile Screen Design

--- app/src/main/java/com/hms/quickline/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hms/quickline/ui/home/HomeFragment.kt
@@ -96,6 +96,7 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
                 }
 
                 startActivity(intent)
+                //navigate(HomeFragmentDirections.actionHomeFragmentToSplash())
             }
         }
     }
