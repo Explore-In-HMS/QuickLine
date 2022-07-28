7dae41a46cf1e63e11ab079dde7afe7ae5a984a9
b84251775 <123ewqaz!>
2022-07-28 10:05:21 +0300

    COM-34:Notification receiver refactored.

--- app/src/main/java/com/hms/quickline/service/NotificationReceiver.kt
+++ app/src/main/java/com/hms/quickline/service/NotificationReceiver.kt
@@ -3,7 +3,9 @@ package com.hms.quickline.service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.hms.quickline.R
 import com.hms.quickline.core.util.Constants
+import com.huawei.hms.feature.dynamic.b.u
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
@@ -11,10 +13,10 @@ class NotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         val uid = intent.getStringExtra(Constants.UID)
-        val callerName = intent.getStringExtra(Constants.CALLER_NAME)
+        val callerName = intent.getStringExtra(Constants.CALLER_NAME) ?: context.resources.getString(R.string.unknown)
 
-        val notificationUtils = callerName?.let { NotificationUtils(context,uid, it) }
-        val notification = notificationUtils?.getNotificationBuilder()?.build()
-        notificationUtils?.getManager()?.notify(150, notification)
+        val notificationUtils = NotificationUtils(context,uid, callerName)
+        val notification = notificationUtils.getNotificationBuilder().build()
+        notificationUtils.getManager().notify(150, notification)
     }
 }
