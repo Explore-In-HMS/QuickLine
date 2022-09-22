ad955784de57534dade9d0cede6c691f8f895637
sinan.yilmaz <sinan.yilmaz3@huawei.com>
2022-09-22 14:05:59 +0300

    Update Copyrights

--- app/src/main/java/com/hms/quickline/ui/recentcalls/RecentCallsFragment.kt
+++ app/src/main/java/com/hms/quickline/ui/recentcalls/RecentCallsFragment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2020-2022. All rights reserved.
+ * Copyright 2022. Explore in HMS. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package com.hms.quickline.ui.recentcalls
